0bb8434b55ec3f14f7d900247cbf22ac16d84fa8
Wilson Berkow <wilsonberkow@gmail.com>
2018-06-14 20:35:04 -0400

    Make setup.sh generate reader-data/endpoint.json

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # fake-elm-package
 
+Move (or symlink) the Elm package `reader` to `./reader`, and to
+generate `reader-data/endpoint.json`, run
+
+    $ ./setup.sh
+
 Build and run (through Docker) with:
 
-    make
+    $ make
--- setup.sh
+++ setup.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
 
+# Generic error handling header, credit to https://stelfox.net/blog/2013/11/fail-fast-in-bash-scripts/
+function error_handler() {
+  echo "Error occurred in script at line: ${1}."
+  echo "Line exited with status: ${2}"
+}
+trap 'error_handler ${LINENO} $?' ERR
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o nounset
+
+# Check the reader/ directory exists
 if [[ ! -d reader ]]; then
   echo "Clone the appropriate version of the reader elm package to ./reader so I can ZIP it"
   exit 1
 fi
 
+# ZIP it. Follow the symlink if there is one because otherwise `zip` returns 18 (an error),
+# despite working, warning that there is a directory and file with the same name.
 echo "ZIPing reader/ to reader.zip (following symlinks; excluding .git/ and elm-stuff/)"
-zip --exclude '*.git*' --exclude '*elm-stuff*' --symlinks -r reader.zip reader/
+if [[ -L reader ]]; then
+  readerpath=$(readlink reader)
+else
+  readerpath=reader
+fi
+zip --exclude '*.git*' --exclude '*elm-stuff*' --symlinks -r reader.zip "$readerpath"
 
-echo "Printing SHA1 hash of reader.zip:"
-shasum reader.zip
 
-echo "Write that hash to the appropriate field in reader-data/endpoint.json"
+# Hash it
+echo # space after `zip` output
+echo "Hashing the ZIP"
+if command -v shasum &> /dev/null; then
+  shacmd=shasum
+elif command -v sha1sum &> /dev/null; then
+  shacmd=sha1sum
+else
+  echo
+  echo "Oh no, neither 'shasum' nor 'sha1sum' are available programs."
+  echo "Quitting without updating reader-data/endpoint.json"
+  exit 1
+fi
+readerhash=$($shacmd reader.zip | cut -f 1 -d " ")
+echo "SHA-1 hash of reader.zip is $readerhash"
+
+
+# Update endpoint.json
+echo "Updating reader-data/endpoint.json"
+echo "{
+    \"url\": \"http://localhost:8080/reader.zip\",
+    \"hash\": \"$readerhash\"
+}" > reader-data/endpoint.json
